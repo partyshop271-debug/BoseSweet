831980b5d993d49b0a4d05f02a64b0d7f3424007
partyshop271-debug <partyshop271@gmail.com>
2026-05-09 12:05:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'bose-sweets-cache-v5';
-const DYNAMIC_CACHE = 'bose-sweets-dynamic-v5';
+const CACHE_NAME 'bose-sweets-dynamic-v6';
+const DYNAMIC_CACHE ='bose-sweets-dynamic-v6';
 const OFFLINE_URL = '/offline.html';
 const ASSETS_TO_CACHE = [
   '/',
